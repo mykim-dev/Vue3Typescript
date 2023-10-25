f3280fd113626f661d1513b310f4306db2f3dee8
mykim-dev <pandagom79@gmail.com>
2023-10-25 22:02:33 +0900

    study

--- src/study/vBind.vue
+++ src/study/vBind.vue
@@ -19,6 +19,38 @@ const TabChange = (current) => {
     <div v-for="c in tmpData.pages" v-if="tmpData.page === c">
         <div>{{ c }} contents</div>
     </div>
+    <!-- <code>
+                        부모 -> 자식 = props 이용
+                        부모 :nameofChild="name" -> 자식 = props: ['nameofChild']
+
+                        props: ['nameofChild']
+                        props: {
+                            nameofChild: String
+                        }
+
+                        props: {
+                            nameofChild: {
+                                type: String,
+                                required: true,
+                                default: 'Lego'
+                            }
+                        }
+
+
+                        부모 -> 자식 = emit 이용
+
+
+                        자식 -> 부모
+                        자식 - this.$emit('child', 전달할 값)
+                        부모 - @child="parents"
+
+
+
+                        형제 -> 형제
+                        eventBus.on
+
+                        컴포넌트에서 공통적으로 사용하는함수 모아놓는것 mixin
+                        </code> -->
 </template>
 
 <style lang="scss" scoped>
