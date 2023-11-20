206a55c8de7c814bf404f6aa852fe4df6919d804
mykim-dev <pandagom79@gmail.com>
2023-11-20 19:53:01 +0900

    20231119

--- src/containers/evMenu.vue
+++ src/containers/evMenu.vue
@@ -16,91 +16,51 @@ const AppSetting = AppSettingStore()
 
 <template>
   <div class="ev-menu">
-    <el-button
-      class="ev-menu-toggle"
-      :icon="AppSetting.menuCollapse === true ? 'DArrowRight' : 'DArrowLeft'"
-      @click="AppSetting.changeMenuCollapse"
-    />
+    <el-button class="ev-menu-toggle" :icon="AppSetting.menuCollapse === true ? 'DArrowRight' : 'DArrowLeft'"
+      @click="AppSetting.changeMenuCollapse" />
     <el-scrollbar always class="ev-menu-scroll">
-      <el-menu :mode="AppSetting.menuType" :collapse="AppSetting.menuCollapse" default-active="2">
-        <el-menu-item index="/">
-          <el-icon>
-            <HomeFilled />
-          </el-icon>
-          <template #title>Home</template>
-        </el-menu-item>
-        <el-sub-menu index="2">
-          <template #title>
-            <el-icon>
-              <Tools />
-            </el-icon>
-            <span>Setting</span>
-          </template>
-          <el-menu-item-group title="Setting">
-            <el-menu-item index="/Setting">Setting</el-menu-item>
-            <el-menu-item index="/NewAdditions">NewAdditions</el-menu-item>
-          </el-menu-item-group>
-        </el-sub-menu>
-        <el-sub-menu index="3">
-          <template #title>
-            <el-icon>
-              <Grid />
-            </el-icon>
-            <span>Screen</span>
-          </template>
-          <el-menu-item-group title="Screen">
-            <el-menu-item index="/Default">Default</el-menu-item>
-            <el-menu-item index="/Vertical">Vertical</el-menu-item>
-            <el-menu-item index="/Horizontal">Horizontal</el-menu-item>
-            <el-menu-item index="/Vertical2">Vertical2</el-menu-item>
-            <el-menu-item index="/Horizontal2">Horizontal2</el-menu-item>
-            <el-menu-item index="/Horizontal3">Horizontal3</el-menu-item>
-            <el-menu-item index="/Dashboard">Dashboard</el-menu-item>
-            <el-menu-item index="/Dashboard1">Dashboard1</el-menu-item>
-            <el-menu-item index="/Dashboard2">Dashboard2</el-menu-item>
-            <el-menu-item index="/Dashboard3">Dashboard3</el-menu-item>
-            <el-menu-item index="/Dynamic">Dynamic</el-menu-item>
-          </el-menu-item-group>
-        </el-sub-menu>
-        <el-sub-menu index="4">
-          <template #title>
-            <el-icon>
-              <Menu />
-            </el-icon>
-            <span>Component</span>
-          </template>
-          <el-menu-item-group title="Component">
-            <el-menu-item index="/Form">Form</el-menu-item>
-            <el-menu-item index="/ToolbarForm">ToolbarForm</el-menu-item>
-            <el-menu-item index="/List">List</el-menu-item>
-            <el-menu-item index="/ToolbarList">ToolbarList</el-menu-item>
-            <el-menu-item index="/Tabs">Tabs</el-menu-item>
-          </el-menu-item-group>
-        </el-sub-menu>
-        <el-sub-menu index="5">
-          <template #title>
-            <el-icon>
-              <QuestionFilled />
-            </el-icon>
-            <span>Sample</span>
-          </template>
-          <el-menu-item-group title="SampleForm">
-            <el-menu-item index="/SampleForm">SampleForm</el-menu-item>
-            <el-menu-item index="/SampleList">SampleList</el-menu-item>
-            <el-menu-item index="/FloatGrid">DisplayBlockGrid</el-menu-item>
-          </el-menu-item-group>
-        </el-sub-menu>
-        <!-- <el-sub-menu index="6">
-        <template #title>
-          <el-icon>
-            <QuestionFilled />
-          </el-icon>
-          <span>Study</span>
+      <el-menu :mode="AppSetting.menuType" :collapse="AppSetting.menuCollapse" default-active="2" router="true">
+        <template v-for="menus in AppSetting.menus">
+
+          <el-menu-item v-if="menus.menus.length === 0" :index="menus.link">
+            <template #title>
+              <el-button :icon="menus.icon" link />
+              {{ menus.name }}
+            </template>
+          </el-menu-item>
+
+          <el-sub-menu v-else :index="menus.link">
+            <template #title>
+              <el-button :icon="menus.icon" link />
+              {{ menus.name }}
+            </template>
+
+            <template v-for="submenus in menus.menus">
+              <el-menu-item v-if="submenus.menus.length === 0" :index="submenus.link">
+                <template #title>
+                  <el-button :icon="submenus.icon" link />
+                  {{ submenus.name }}
+                </template>
+              </el-menu-item>
+
+              <el-sub-menu v-else :index="submenus.link">
+
+                <template #title>
+                  <el-button :icon="submenus.icon" link />
+                  {{ submenus.name }}
+                </template>
+
+                <el-menu-item-group :title="submenus.name">
+                  <el-menu-item :index="submenus.link">
+                    {{ submenus.name }}
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-sub-menu>
+
+            </template>
+          </el-sub-menu>
+
         </template>
-        <el-menu-item-group title="Study">
-          <el-menu-item index="/Bind">bind</el-menu-item>
-        </el-menu-item-group>
-      </el-sub-menu> -->
       </el-menu>
     </el-scrollbar>
     <div class="ev-menu__banner">
--- src/stores/common/index.ts
+++ src/stores/common/index.ts
@@ -10,51 +10,55 @@ export const AppSettingStore = defineStore('AppSetting', {
         menuType: ref('vertical'),
         menuCollapse: ref(false),
         menus: [
-            { name: 'Home', link: '/', icon: '', submenu: [] },
-            { name: 'Setting', link: '/Setting', icon: 'Setting', submenu: [] },
+            { name: 'Home', link: '/', icon: 'HomeFilled', menus: [] },
+            { name: 'Setting', link: '/Setting', icon: 'Setting', menus: [] },
             {
-                name: 'Screen',
-                link: '/Default',
-                icon: 'Menu',
-                submenu: [
-                    { name: 'Default', link: '/Default', icon: 'Menu', submenu: [] },
+                name: 'Screen', link: '/Default', icon: 'FullScreen',
+                menus: [
+                    { name: 'Default', link: '/Default', icon: '', menus: [] },
                     {
-                        name: 'Horizontal',
-                        link: '/Horizontal',
-                        icon: 'Location',
-                        submenu: [],
+                        name: 'Horizontal', link: '/Horizontal', icon: '', menus: [
+                            { name: 'Default', link: '/Default', icon: '', menus: [] },
+                            {
+                                name: 'Horizontal',
+                                link: '/Horizontal',
+                                icon: 'Location',
+                                menus: [],
+                            }
+                        ],
                     },
-                    { name: 'Vertical', link: '/Vertical', icon: 'Setting', submenu: [] },
-                    { name: 'Horizontal2', link: '/Horizontal2', icon: 'Menu', submenu: [] },
-                    { name: 'Vertical2', link: '/Vertical2', icon: 'Location', submenu: [] },
+                    { name: 'Vertical', link: '/Vertical', icon: '', menus: [] },
+                    { name: 'Horizontal2', link: '/Horizontal2', icon: '', menus: [] },
+                    { name: 'Vertical2', link: '/Vertical2', icon: '', menus: [] },
                     {
                         name: 'Horizontal3',
                         link: '/Horizontal3',
-                        icon: 'Setting',
-                        submenu: [],
+                        icon: '',
+                        menus: [],
                     },
-                    { name: 'Dashboard', link: '/Dashboard', icon: 'Menu', submenu: [] },
+                    { name: 'Dashboard', link: '/Dashboard', icon: '', menus: [] },
                     {
                         name: 'Dashboard1',
                         link: '/Dashboard1',
-                        icon: 'Location',
-                        submenu: [],
+                        icon: '',
+                        menus: [],
                     },
-                    { name: 'Dashboard2', link: '/Dashboard2', icon: 'Setting', submenu: [] },
-                    { name: 'Dashboard3', link: '/Dashboard3', icon: 'Menu', submenu: [] },
-                    { name: 'Dynamic', link: '/Dynamic', icon: 'Location', submenu: [] },
+                    { name: 'Dashboard2', link: '/Dashboard2', icon: '', menus: [] },
+                    { name: 'Dashboard3', link: '/Dashboard3', icon: '', menus: [] },
+                    { name: 'Dynamic', link: '/Dynamic', icon: '', menus: [] },
                 ],
             },
             {
-                name: 'Component', link: '/', icon: 'Setting', submenu: [
-                    { name: 'Form', link: '/Form', icon: '', submenu: [] },
-                    { name: 'List', link: '/List', icon: '', submenu: [] },
-                    { name: 'ToolbarForm', link: '/ToolbarForm', icon: '', submenu: [] },
-                    { name: 'ToolbarList', link: '/ToolbarList', icon: '', submenu: [] },
-                    { name: 'Tabs', link: '/Tabs', icon: '', submenu: [] },
+                name: 'Component', link: '/', icon: 'Grid', menus: [
+                    { name: 'Form', link: '/Form', icon: '', menus: [] },
+                    { name: 'List', link: '/List', icon: '', menus: [] },
+                    { name: 'ToolbarForm', link: '/ToolbarForm', icon: '', menus: [] },
+                    { name: 'ToolbarList', link: '/ToolbarList', icon: '', menus: [] },
+                    { name: 'Tabs', link: '/Tabs', icon: '', menus: [] },
                 ]
             },
-            { name: 'Control', link: '/', icon: 'Menu', submenu: [] },
+            { name: 'Control', link: '/', icon: 'Mouse', menus: [] },
+
         ],
         tags: [
             { name: 'SampleForm', link: '/SampleForm' },
