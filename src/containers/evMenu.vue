7bb8dedd28d2ce8c5f8c6f3495ee617e5be89048
mykim-dev <pandagom79@gmail.com>
2023-11-21 22:56:53 +0900

    menu test

--- src/containers/evMenu.vue
+++ src/containers/evMenu.vue
@@ -18,102 +18,61 @@ const AppSetting = AppSettingStore()
   <div class="ev-menu">
     <el-button class="ev-menu-toggle" :icon="AppSetting.menuCollapse === true ? 'DArrowRight' : 'DArrowLeft'"
       @click="AppSetting.changeMenuCollapse" />
-    <!-- <el-scrollbar always class="ev-menu-scroll"> -->
-      <!-- <el-menu :mode="AppSetting.menuType" collapse="true" default-active="2" router="false" teleported="false">
-        <el-sub-menu v-for="a in 10" :index="`a${a}`">
+    <el-scrollbar always class="ev-menu-scroll">
+      <el-menu :mode="AppSetting.menuType" collapse="true" router="false">
+        <el-sub-menu v-for="a in 1" :index="`a${a}`" :popper-class="`subMain subMain${a}`">
           <template #title>{{ `a${a}` }}</template>
             <el-sub-menu v-for="b in 10" :index="`b${a}-${b}`">
               <template #title>{{ `b${a}-${b}` }}</template>
               <el-menu-item :index="`${a}-${b}`">{{ `c${a}-${b}` }}</el-menu-item>
-              <el-sub-menu v-for="c in 2" :index="`${a}-${b}-${c}`">
+              <el-sub-menu v-for="c in 1" :index="`${a}-${b}-${c}`">
                 <template #title>{{ `c${a}-${b}-${c}` }}</template>
-                <el-sub-menu v-for="d in 2" :index="`${a}-${b}-${c}-${d}`">
+                <el-sub-menu v-for="d in 1" :index="`${a}-${b}-${c}-${d}`">
                   <template #title>{{ `d${a}-${b}-${c}-${d}` }}</template>              
                   <el-menu-item :index="`${a}-${b}-${c}-${d}`">{{ `e${a}-${b}-${c}-${d}` }}</el-menu-item>
                 </el-sub-menu>
               </el-sub-menu>
             </el-sub-menu>
         </el-sub-menu>
-      </el-menu> -->
-      <div class="menuTest collapse">
-        <!-- <ul v-for="a in 5" :class="`deaph1`">
-          <li>
-            <span class="title">{{ `${a}` }}</span>
-            <ul v-for="b in 5" :class="`deaph2`">
-              <li>{{ `${a}-${b}` }}</li>
-              <ul v-for="c in 5" :class="`deaph3`">
-                <li>{{ `${a}-${b}-${c}` }}</li>
-              </ul>
-            </ul>
-
-          </li>
-          
-        </ul> -->
-      </div>
-    <!-- </el-scrollbar> -->
+      </el-menu>
+    </el-scrollbar>
     <div class="ev-menu__banner">
       <img src="@/assets/img/menu-banner.png" alt="하단배너" />
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.menuTest {
+.menu-test {
+  border: 1px solid red;
+  height: 100%;
+  
   ul, li {
     list-style: none;
     padding: 0;
     margin: 0;
   }
+  .sub-menu {
+    width: 200px;
+    position: absolute;
+    border: 1px solid green;
 
-  // ul {
-  //   max-height: 60vh;
-  //   overflow: auto;
-  //   border: 1px solid green;
-  // }
-
-  // li {
-  //   display: flex;
-  //   align-items: center;
-  //   min-height: 50px;
-  //   border: 1px solid blue;
-  // }
-
-  &.collapse {
-    width: 660px;
-    height: 100%;
-    z-index: 99;
-    border: 1px solid cyan;
-
-
-    ul {
-      max-height: 60vh;
-      // overflow: auto;
-      border: 1px solid green;
+    &.sub-menu1 {
+      left: 70px;
     }
 
-    li {
-      display: flex;
-      align-items: center;
-      width: 200px;      
-      min-height: 50px;
-      border: 1px solid blue;
+    &.sub-menu2 {
+      left: 200px;
     }
 
-    ul {
-      &[class*="deaph1"] {
-        left: 0;
-      }
-
-      &[class*="deaph2"] {
-        width: 200px;
-        position: absolute;
-        left: 200px;
-      }
-
-      &[class*="deaph3"] {        
-        position: absolute;
-        left: 200px;
-      }
+    &.sub-menu3 {
+      left: 200px;
     }
+
+    &.sub-menu4 {
+      left: 200px;
+    }
+
   }
-  }
+}
+
 </style>
