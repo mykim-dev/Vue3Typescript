3532aee151bea535b7abaff212e2c4f04c891990
mykim-dev <pandagom79@gmail.com>
2023-12-05 23:06:33 +0900

    overlay

--- src/setting/overlayAction.vue
+++ src/setting/overlayAction.vue
@@ -12,19 +12,13 @@
       <i class="c"></i><i class="a"></i><i class="l"></i><i class="s"></i>
     </div>
     <div class="loading__icon cloud">
-      <i></i>
+      <i></i><i></i><i></i><i></i><i></i><i></i><i></i>
     </div>
     <div class="loading__icon ellipsis">
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
+      <i></i><i></i><i></i><i></i><i></i>
     </div>
     <div class="loading__icon gear">
-      <i></i>
-      <i></i>
-      <i></i>
+      <i></i><i></i><i></i>
     </div>
     <div class="loading__icon straight"><i></i></div>
   </div>
@@ -74,14 +68,15 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 18%;
-    height: 150px;
+    width: 320px;
+    height: 160px;
     border: 1px solid white;
     position: relative;
 
     & > i {
       display: block;      
       position: relative;
+      // border: 1px solid red;
     }
 
     &.basic {
@@ -98,44 +93,99 @@
 
       i {
         display: flex;
-        justify-content: center;        
-        height: 40%;
-        margin-top: 20%;
+        justify-content: center;
+        height: 60px;
+        margin-top: 55px;
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center center;
         opacity: 0;
-        animation-duration: 4s;
-        animation-timing-function: linear;
+        animation-duration: 3s;
+        animation-timing-function: ease-in-out;
         animation-iteration-count: infinite;
 
         &.c {
-          width: 20%;
+          width: 60px;
           background-image: url('data:image/svg+xml,<svg viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.0332 17.2704C12.5165 17.1949 13.6915 16.6407 14.6249 15.7758C15.5665 14.9109 16.1915 13.727 16.4249 12.2323L19.8665 11.9972C19.6332 14.3567 18.6915 16.33 17.1332 17.9002C15.5665 19.3948 13.6165 20.2597 11.2749 20.4192C8.54153 20.6544 6.11653 19.8651 4.0832 18.1353C2.04986 16.4056 0.958194 14.1216 0.724861 11.2835C0.491528 8.45376 1.27486 6.01029 2.99153 3.96146C4.79153 1.91264 7.04986 0.812651 9.79153 0.577539C12.1332 0.418 14.2415 0.972191 15.9665 2.23172C17.7665 3.56681 18.9332 5.30496 19.5582 7.66446L16.1165 7.89957C15.6499 6.48051 14.8665 5.38053 13.8499 4.67519C12.8332 3.96986 11.5082 3.65078 10.0249 3.72635C8.14986 3.88589 6.66653 4.59123 5.57486 5.92632C4.48319 7.26142 3.9332 8.91559 4.09153 10.8888C4.24986 12.8621 4.94986 14.3567 6.27486 15.5323C7.59986 16.8674 9.16653 17.4216 11.0415 17.262L11.0332 17.2704Z" fill="white"/></svg>');
-          animation-name: movebasicC;
-          animation-delay: 1s;
+          animation-name: movebasicC;          
         }
         &.a {
-          width: 20%;
+          width: 60px;
           background-image: url('data:image/svg+xml,<svg viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.2837 12.8606L10.0003 5.14393L6.80032 12.8606L5.47532 16.0094L3.75866 20.2582H0.166992L8.52532 0.970703H11.6503L19.9337 20.1826H16.417L14.542 15.9338L13.292 12.8606H13.2837Z" fill="white"/></svg>');
           animation-name: movebasicA;
-          animation-delay: 2s;
         }
         &.l {
-          width: 15%;
+          width: 45px;
           background-image: url('data:image/svg+xml,<svg viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.2335 16.1192V16.9085C14.2335 18.168 13.2169 19.1085 12.0419 19.1085H0.950195V2.26442C0.950195 1.0049 1.96687 0.0644531 3.14187 0.0644531H4.23353V16.1276H14.2335V16.1192Z" fill="white"/></svg>');
           animation-name: movebasicL;
-          animation-delay: 3s;
         }
         &.s {
-          width: 17%;
-          background-image: url('data:image/svg+xml,<svg width="17" height="21" viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.29981 0.265625C9.15814 0.265625 10.0165 0.341196 10.8748 0.584704C11.7331 0.819815 12.5165 1.1389 13.2165 1.53355C13.9165 1.9282 14.6248 2.63353 15.0915 3.42283C15.5581 4.21213 15.8748 5.15258 16.0248 6.33653H12.7415C12.4248 4.21213 11.0248 3.18772 8.5998 3.18772C7.4248 3.18772 6.56647 3.42283 5.86647 3.89305C5.16647 4.36328 4.8498 5.07701 4.8498 5.94188C4.8498 6.64721 5.08314 7.20141 5.5498 7.59606C6.01647 7.99071 6.7998 8.30139 7.89147 8.5449L10.3165 9.01512C12.2665 9.40977 13.6748 10.0395 14.6165 10.8288C15.5498 11.6937 16.0248 12.7937 16.0248 14.2128C16.0248 16.0265 15.3248 17.4371 13.9915 18.5455C12.6581 19.6539 10.9415 20.1997 8.6748 20.1997C6.40814 20.1997 4.45813 19.5699 2.9748 18.4699C1.56647 17.286 0.708138 15.6402 0.549805 13.5914H3.83314C3.99147 14.6914 4.53314 15.6402 5.3998 16.27C6.25814 16.8997 7.43314 17.2188 8.84147 17.2188C10.0165 17.2188 10.8748 16.9837 11.5748 16.5135C12.1998 16.0433 12.5915 15.4135 12.5915 14.6242C12.5915 13.4402 11.6581 12.6593 9.78314 12.2647L6.65814 11.6349C3.06647 10.9296 1.34147 9.11588 1.34147 6.20218C1.34147 4.38847 1.96647 2.9778 3.29147 1.95339C4.46647 0.853402 6.18314 0.299211 8.29147 0.299211L8.29981 0.265625Z" fill="white"/></svg>');
+          width: 51px;
+          background-image: url('data:image/svg+xml,<svg viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.29981 0.265625C9.15814 0.265625 10.0165 0.341196 10.8748 0.584704C11.7331 0.819815 12.5165 1.1389 13.2165 1.53355C13.9165 1.9282 14.6248 2.63353 15.0915 3.42283C15.5581 4.21213 15.8748 5.15258 16.0248 6.33653H12.7415C12.4248 4.21213 11.0248 3.18772 8.5998 3.18772C7.4248 3.18772 6.56647 3.42283 5.86647 3.89305C5.16647 4.36328 4.8498 5.07701 4.8498 5.94188C4.8498 6.64721 5.08314 7.20141 5.5498 7.59606C6.01647 7.99071 6.7998 8.30139 7.89147 8.5449L10.3165 9.01512C12.2665 9.40977 13.6748 10.0395 14.6165 10.8288C15.5498 11.6937 16.0248 12.7937 16.0248 14.2128C16.0248 16.0265 15.3248 17.4371 13.9915 18.5455C12.6581 19.6539 10.9415 20.1997 8.6748 20.1997C6.40814 20.1997 4.45813 19.5699 2.9748 18.4699C1.56647 17.286 0.708138 15.6402 0.549805 13.5914H3.83314C3.99147 14.6914 4.53314 15.6402 5.3998 16.27C6.25814 16.8997 7.43314 17.2188 8.84147 17.2188C10.0165 17.2188 10.8748 16.9837 11.5748 16.5135C12.1998 16.0433 12.5915 15.4135 12.5915 14.6242C12.5915 13.4402 11.6581 12.6593 9.78314 12.2647L6.65814 11.6349C3.06647 10.9296 1.34147 9.11588 1.34147 6.20218C1.34147 4.38847 1.96647 2.9778 3.29147 1.95339C4.46647 0.853402 6.18314 0.299211 8.29147 0.299211L8.29981 0.265625Z" fill="white"/></svg>');
           animation-name: movebasicS;
-          animation-delay: 4s;
         }
       }
     }
     &.cloud {
+      & > i {
+        display: block;
+        position: absolute;
+        bottom: 30px;
+        left: 50%;
+        background-color: white;
+        animation-duration: 1s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        
+        &:nth-of-type(1) {
+          width: 80px;
+          height: 35px;
+          margin-left: -55px;
+          border-radius: 25px;
+          animation-name: moveCloudSizeOne;
+        }
+
+        &:nth-of-type(2) {
+          width: 90px;
+          height: 40px;
+          margin-left: -25px;
+          border-radius: 30px;
+          animation-name: moveCloudSizeTwo;
+        }
+
+        &:nth-of-type(3) {
+          width: 70px;
+          height: 70px;
+          margin-left: -35px;
+          border-radius: 50%;
+          animation-name: moveCloudSizeThree;          
+        }
+
+        &:nth-of-type(4),
+        &:nth-of-type(5),
+        &:nth-of-type(6) {
+          width: 10px;
+          height: 10px;
+          margin-left: -5px;
+          border-radius: 5px;
+        }
+
+        &:nth-of-type(4) {
+          animation-name: moveCloudSizeFour;
+        }
+
+        &:nth-of-type(5) {
+          animation-name: moveCloudSizeFive;
+        }
+
+        &:nth-of-type(6) {
+          animation-name: moveCloudSizeSix;
+        }
+
+        &:nth-of-type(7) {
+          // animation-delay: -0.2s;
+        }
+      }
     }
     &.ellipsis {
       & > i {
@@ -181,8 +231,8 @@
       i {
         display: block;
         width: 70%;
-        height: 3px;
-        border-radius: 2px;
+        height: 5px;
+        border-radius: 3px;
         background-color: #eeeeee;
         overflow: hidden;
 
@@ -191,7 +241,7 @@
           content: "";
           width: 0;
           height: 100%;
-          background-color: #6edc00;
+          background-color: var(--el-color-primary);
           animation: moveStraight 3s ease-out infinite;
         }
       }
@@ -200,39 +250,60 @@
 }
 
 @keyframes movebasicC {
-  0% {opacity: 0;}
-  10% {opacity: 1;transform: rotate(45deg);}
-  20% {opacity: 1;transform: rotate(0deg);}
-  100% {opacity: 1;}
+  10% {opacity: 0;}
+  20% {opacity: 1;transform: rotate(45deg);}
+  30% {opacity: 1;transform: rotate(0deg);}
+  85% {opacity: 1;}
+  90% {opacity: 0;}
 }
 @keyframes movebasicA {
-  0% {opacity: 0;}
-  10% {opacity: 1;transform: rotate(45deg);}
-  20% {opacity: 1;transform: rotate(0deg);}
-  100% {opacity: 1;}
+  20% {opacity: 0;}
+  30% {opacity: 1;transform: rotate(0deg);}
+  40% {opacity: 1;transform: rotate(30deg) translate(10px, -20px);}
+  50% {opacity: 1;transform: rotate(0deg);}
+  85% {opacity: 1;}
+  90% {opacity: 0;}
 }
 @keyframes movebasicL {
-  0% {opacity: 0;}
-  10% {opacity: 1;transform: translate(0, -20%);}
-  20% {opacity: 1;transform: translate(0, 0);}
-  100% {opacity: 1;}
+  35% {opacity: 0;}
+  45% {opacity: 1;transform: translate(0, 0);}
+  55% {opacity: 1;transform: translate(0, -30px);}
+  65% {opacity: 1;transform: translate(0, 0);}
+  85% {opacity: 1;}
+  90% {opacity: 0;}
 }
 @keyframes movebasicS {
-  0% {opacity: 0;}
-  10% {opacity: 1;transform: scale(1.1);}
-  20% {opacity: 1;transform: initial;}
-  100% {opacity: 1;}
+  60% {opacity: 0;}
+  65% {opacity: 1;transform: scale(1);}
+  70% {opacity: 1;transform: scale(1.15);}  
+  80% {opacity: 1;transform: scale(1);}
+  85% {opacity: 1;}
+  90% {opacity: 0;}
 }
-
+@keyframes moveCloudSizeOne {
+  20% {height: 40px;}
+  25% {transform: translate(-5px, 0);}
+  30% {height: 35px;transform: initial;}
+}
+@keyframes moveCloudSizeTwo {
+  30% {height: 50px;}
+  35% {transform: translate(5px, 0);}
+  40% {height: 40px;transform: initial;}
+}
+@keyframes moveCloudSizeThree {
+  0% {width: 80px;height: 80px;margin-left:-40px;}
+  20% {width: 70px;height: 70px;margin-left:-35px;}
+}
+@keyframes moveCloudSizeFour {
+  10% {transform: translate(-80px, -45px);}
+  20% {opacity: 0;}
+}
 @keyframes moveEllipsis {
   0% {transform: scale(1);}
-	100% {transform: scale(8);}
+	100% {transform: scale(7);background-color: var(--el-color-primary-dark-2);}  
 }
-
 @keyframes moveStraight {
   0% {width: 0;}
 	100% {width: 102%;}
 }
-
-
 </style>
